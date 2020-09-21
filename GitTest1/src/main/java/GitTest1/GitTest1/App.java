157ec36fabe02aae5ecc21ca57f38a1907f6a674
Sonal Choudhari <sonal.choudhari@gmail.com>
2020-09-21 19:11:48 -0400

    First Commit with New Project

---
+++ GitTest1/src/main/java/GitTest1/GitTest1/App.java
@@ -0,0 +1,13 @@
+package GitTest1.GitTest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
